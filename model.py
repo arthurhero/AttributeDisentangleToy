fb5db8dc8ef53dac21a14b4a894802b4accdbf5c
arthurhero <chenziwe@grinnell.edu>
2021-03-06 15:11:31 +0800

    self attn

--- model.py
+++ model.py
@@ -12,11 +12,11 @@ class ADNet(nn.Module):
         self.resnet50 = models.resnet50(pretrained=True)
         self.resnet50.conv1 = nn.Conv2d(in_channels,64,kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         #num_ftrs = self.resnet50.fc.in_features
-        self.resnet50.fc = nn.Linear(1024, 1024)
+        self.resnet50.fc = nn.Linear(num_atrs*49, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
         self.atr_vec = nn.Parameter(torch.randn(1024,num_atrs))
         self.q_encoder = nn.Linear(1024,1024)
-        self.k_encoder = nn.Linear(1024,1024)
-        self.attn = nn.MultiheadAttention(1024, num_heads=1)
+        self.k_encoder = nn.Linear(49,1024)
 
     def forward(self, x):
         '''
@@ -31,11 +31,13 @@ class ADNet(nn.Module):
             x = v(x)
             if 'layer4' in k:
                 last_feat = x.clone() # b x 2048 x h' x w'
-                last_feat = F.interpolate(last_feat,(32,32),mode='bilinear').reshape(b,2048,-1).permute(1,0,2) # 2048 x b x 1024
-                query = self.q_encoder(self.atr_vec.transpose(0,1).unsqueeze(1).expand(-1,b,-1)) # num_atrs x b x 1024
-                kv = self.k_encoder(last_feat) # 2048 x b x 1024
-                attended,_ = self.attn(query,kv,kv) # num_atrs x b x 1024
-                x = attended.mean(0) # b x 1024 
-        feat_vec = x # b x 1024
+                last_feat = F.interpolate(last_feat,(7,7),mode='bilinear').reshape(b,2048,-1) # b x 2048 x 49
+                key = self.k_encoder(last_feat) # b x 2048 x 1024 
+                query = (self.q_encoder(self.atr_vec.transpose(0,1))).transpose(0,1).unsqueeze(0).expand(b,-1,-1) # b x 1024 x num_atrs
+                weights = F.softmax(torch.bmm(key,query), dim=1) # b x 2048 x num_atrs 
+                attended = last_feat.unsqueeze(2) * weights.unsqueeze(3) # b x 2048 x num_atrs x 49
+                attended = attended.sum(1) # b x num_atrs x 49
+                x = attended.reshape(b,-1) # b x (num_atrs x 49)
+        feat_vec = self.fc2(x) # b x 1024
         sim_vec = feat_vec.unsqueeze(1).bmm(self.atr_vec.unsqueeze(0).expand(b,-1,-1)) # b x 1 x 102
         return torch.sigmoid(sim_vec.squeeze(1)) # b x 102
